24d7714d1d2844de12a95a37a7544d1c85328eaf
MarianoCroce <marianoaguirre14@gmail.com>
2023-10-07 21:30:57 -0300

    Css arreglado.

--- client/src/Components/Form/FormComponent.jsx
+++ client/src/Components/Form/FormComponent.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Styles from "./formComponent.module.css";
+import styles from "./formComponent.module.css"; // Importa los estilos del módulo CSS
 
 const FormComponent = (props) => {
   const {
@@ -16,100 +16,101 @@ const FormComponent = (props) => {
   } = props;
 
   return (
-    <div className={Styles.formContainer}>
-        
-        <form className={Styles.formOptions}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            value={input.name}
-            onChange={handleChange}
-          />
-          {errorMessage.name && <span>{errorMessage.name}</span>}
-          <label htmlFor="difficulty">Difficulty</label>
-          <select
-            name="difficulty"
-            value={input.difficulty}
-            onChange={handleChange}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-          <label htmlFor="duration">Duration (hours)</label>
-          <select
-            name="duration"
-            value={input.duration}
-            onChange={handleChange}
-          >
-            {Array.from({ length: 24 }, (_, i) => (
-              <option key={i} value={i + 1}>
-                {i + 1}
-              </option>
-            ))}
-          </select>
-          <label htmlFor="season">Season</label>
-          <select name="season" value={input.season} onChange={handleChange}>
-            <option value="Summer">Summer</option>
-            <option value="Autumn">Autumn</option>
-            <option value="Winter">Winter</option>
-            <option value="Spring">Spring</option>
-          </select>
+    <div className={styles.formContainer}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          value={input.name}
+          onChange={handleChange}
+        />
+        {errorMessage.name && <span>{errorMessage.name}</span>}
+        <label htmlFor="difficulty">Difficulty</label>
+        <select
+          name="difficulty"
+          value={input.difficulty}
+          onChange={handleChange}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <label htmlFor="duration">Duration (hours)</label>
+        <select
+          name="duration"
+          value={input.duration}
+          onChange={handleChange}
+        >
+          {Array.from({ length: 24 }, (_, i) => (
+            <option key={i} value={i + 1}>
+              {i + 1}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="season">Season</label>
+        <select name="season" value={input.season} onChange={handleChange}>
+          <option value="Summer">Summer</option>
+          <option value="Autumn">Autumn</option>
+          <option value="Winter">Winter</option>
+          <option value="Spring">Spring</option>
+        </select>
+
           <label htmlFor="countries">Countries</label>
           <input type="text" onChange={handleSearch} value={searchTerm} />
-        </form>
-        
-      <div className={Styles.form} onSubmit={handleSubmit}>
-            <div className={Styles.countries}>
-                <ul>
-                    {filteredCountries.map((country) => (
-                    <li key={country.id}>
-                        {country.name}{" "}
-                        <button
-                        type="button"
-                        onClick={() => handleCountrySelect(country)}
-                        >
-                        +
-                        </button>
-                    </li>
-                    ))}
-                </ul>
-                <div className={Styles.countriesToAdd}>
-                        {input.countries.map((country, index) => (
-                        <span key={index}>
-                            {country}{" "}
-                            <button
-                            type="button"
-                            onClick={() => handleCountryRemove(index)}
-                            >
-                            X
-                            </button>
-                        </span>
-                        ))}
-                </div>
-                {errorMessage.countries && <span>{errorMessage.countries}</span>}
-            </div>
-            {!errorMessage.name &&
-            !errorMessage.countries &&
-            !errorMessage.other &&
-            !success && <button type="submit">Create</button>}
-            {errorMessage.other && !success && <span>{errorMessage.other}</span>}
-            {errorMessage.other && (
-            <button
-                onClick={() => {
-                window.location.reload();
-                }}
-            >
-                Reload
-            </button>
-            )}
-            {success && (
-            <button onClick={() => window.location.reload()}>OK</button>
-            )}
-      </div>
+        <div className={styles.countries}>
+          <ul className={styles.countriesToAdd}>
+            {filteredCountries.map((country) => (
+              <li key={country.id}>
+                {country.name}{" "}
+                <button
+                  type="button"
+                  onClick={() => handleCountrySelect(country)}
+                >
+                  +
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div>
+            {input.countries.map((country, index) => (
+              <span key={index}>
+                {country}{" "}
+                <button
+                  type="button"
+                  onClick={() => handleCountryRemove(index)}
+                >
+                  X
+                </button>
+              </span>
+            ))}
+          </div>
+          {errorMessage.countries && (
+            <span>{errorMessage.countries}</span>
+          )}
+        </div>
+        {!errorMessage.name &&
+          !errorMessage.countries &&
+          !errorMessage.other &&
+          !success && <button type="submit">Create</button>}
+        {errorMessage.other && !success && (
+          <span>{errorMessage.other}</span>
+        )}
+        {errorMessage.other && (
+          <button
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            Reload
+          </button>
+        )}
+        {success && (
+          <button onClick={() => window.location.reload()}>OK</button>
+        )}
+      </form>
       <Link to="/home">
         <button>Home</button>
       </Link>
