e69c02d359e5c951d160ca9e10ba852d43269536
MarianoCroce <marianoaguirre14@gmail.com>
2023-10-12 12:24:59 -0300

    Cambios finales 1.0

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const PORT = 3001;
@@ -6,7 +5,7 @@ const loadCountries = require('./src/utils/loadCountries')
 
 
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
   loadCountries()
